e7f192a041fe184855ed2be98726c90ece26dc35
rolandowebdev <rolandopranata@gmail.com>
2023-09-07 20:44:51 +0700

    chore(db): remove role and verified field from schema prisma

--- prisma/migrations/20230907133814_init/migration.sql
+++ prisma/migrations/20230907133814_init/migration.sql
@@ -4,9 +4,7 @@ CREATE TABLE "User" (
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "role" TEXT DEFAULT 'user',
     "photo" TEXT DEFAULT 'default.png',
-    "verified" BOOLEAN DEFAULT false,
     "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3),
 
